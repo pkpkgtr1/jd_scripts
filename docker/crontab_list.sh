7659acdff31a02c3d9785ce48b2bd9ad443737a7
重新来过²º¹² <pkpkgtr@gmail.com>
2021-06-21 21:21:25 +0800

    增加半点京豆雨

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -30,6 +30,11 @@
 30 1,6,12,21 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 #省钱大赢家之翻翻乐
 10,40 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
+#半点京豆雨
+30 16-23/1 * * *  node /scripts/jd_half_redrain.js >> /scripts/logs/jd_half_redrain.log 2>&1
+
+
+
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
