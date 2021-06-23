b1766e80f472f6f288cd7677e7d0288166216e1e
重新来过²º¹² <pkpkgtr@gmail.com>
2021-06-23 21:36:18 +0800

    fix

--- jd_joy.js
+++ jd_joy.js
@@ -552,7 +552,7 @@
        $.isLogin = true;
        $.nickName = '';
        await TotalBean();
-       if (!require('./JS_USER_AGENTS').HelloWorld) {
+       if (!require('./JS_USER_AGENTS').USER_AGENT) {
          console.log(`\n【京东账号${$.index}】${$.nickName || $.UserName}：运行环境检测失败\n`);
          continue
        }
