1f0a9481212d3050678bdae1acfef39462dc19ea
重新来过²º¹² <pkpkgtr@gmail.com>
2021-06-24 21:05:59 +0800

    删除过期活动

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,8 +4,6 @@
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
 ##############短期活动##############
-#女装盲盒 活动时间：2021-05-24到2021-06-22
-35 1,22 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 
 #京东极速版红包(活动时间：2021-5-5至2021-5-31)
 #45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
