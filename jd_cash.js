ac3768c7a32bc9313707758d02fae7606017eb2b
重新来过²º¹² <pkpkgtr@gmail.com>
2021-07-03 12:41:48 +0800

    fix

--- jd_cash.js
+++ jd_cash.js
@@ -50,7 +50,7 @@ let allMessage = '';
     return;
   }
   await requireConfig()
-  //await getAuthorShareCode();
+  await getAuthorShareCode();
   await getAuthorShareCode2();
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
@@ -459,7 +459,7 @@ function taskUrl(functionId, body = {}) {
   }
 }
 
-function getAuthorShareCode(url = "http://cdn.annnibb.me/jd_cash.json") {
+function getAuthorShareCode(url = "https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/jd_updateCash.json") {
   return new Promise(resolve => {
     $.get({url, headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
