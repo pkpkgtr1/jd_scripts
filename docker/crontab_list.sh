dfbb6684e68c81d040afd8db0a7c0ea18bdf0bfb
重新来过²º¹² <pkpkgtr@gmail.com>
2021-07-12 22:24:59 +0800

    增加宠汪汪偷好友积分与狗粮

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -45,6 +45,8 @@
 15 6-18/6 * * * node /scripts/jd_fruit.js >> /scripts/logs/jd_fruit.log 2>&1
 # 宠汪汪
 45 */2,23 * * * node /scripts/jd_joy.js >> /scripts/logs/jd_joy.log 2>&1
+# 宠汪汪偷好友积分与狗粮
+10 0-21/3 * * * node /scripts/jd_joy_steal.js >> /scripts/logs/jd_joy_steal.log 2>&1
 # 宠汪汪积分兑换京豆
 0 0-16/8 * * * node /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
 # 宠汪汪喂食
