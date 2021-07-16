6b1b7a9af9db4e1f29f925edef3a2039b037497f
重新来过²º¹² <pkpkgtr@gmail.com>
2021-07-16 10:11:32 +0800

    调整惊喜牧场定时任务

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -132,7 +132,7 @@
 # 跳跳乐瓜分京豆
 15 0,12,22 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 #京喜牧场
-15 0,3,6,12,16,20,22 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
+15 0,3,6,12,14,16,18,20,22 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
 #京东电竞经理
 41 10 * * * node /scripts/jd_djjl.js >> /scripts/logs/jd_djjl.log 2>&1
 #柠檬全民挖现金
