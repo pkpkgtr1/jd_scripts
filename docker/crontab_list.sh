768d03d614f016a19d1bfd79abc9da7bde5ab37c
重新来过²º¹² <pkpkgtr@gmail.com>
2021-08-13 22:07:37 +0800

    增加取消关注店铺运行次数

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -105,8 +105,8 @@
 6 0 * * * node /scripts/jd_shop.js >> /scripts/logs/jd_shop.log 2>&1
 # 东东超市
 31 0,1-23/2 * * * node /scripts/jd_superMarket.js >> /scripts/logs/jd_superMarket.log 2>&1
-# 取关京东店铺商品
-45 23 * * * node /scripts/jd_unsubscribe.js >> /scripts/logs/jd_unsubscribe.log 2>&1
+# 取消关注
+45 23,6 * * * node /scripts/jd_unsubscribe.js >> /scripts/logs/jd_unsubscribe.log 2>&1
 # 京豆变动通知
 20 10 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
 # 京东抽奖机
