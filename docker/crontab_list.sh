d97c96efa9be36ccdc6aea6e6b566ebeeb3d5c63
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2021-08-24 10:43:30 +0800

    更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -105,7 +105,9 @@
 # 取消关注
 45 23,6 * * * node /scripts/jd_unsubscribe.js >> /scripts/logs/jd_unsubscribe.log 2>&1
 # 京豆变动通知
-20 10 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
+#20 10 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
+# 京豆变动通知(增强版)
+20 10 * * * node /scripts/jd_bean_change_all.js >> /scripts/logs/jd_bean_change_all.log 2>&1
 # 京东抽奖机
 0 0,12,23 * * * node /scripts/jd_lotteryMachine.js >> /scripts/logs/jd_lotteryMachine.log 2>&1
 # 京东排行榜
