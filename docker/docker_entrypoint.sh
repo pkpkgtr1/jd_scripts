00528edcb1367c20a2c9d9a43574b1f0b00aeafa
重新来过²º¹² <pkpkgtr@gmail.com>
2021-09-07 22:55:31 +0800

    fix

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -13,9 +13,10 @@ else
   echo "设定远程仓库地址..."
   cd /scripts
   git remote set-url origin "$REPO_URL"
-  git reset --hard
+ # git reset --hard
   echo "git pull拉取最新代码..."
-  git -C /scripts pull --rebase
+ # git -C /scripts pull --rebase
+  git pull
   echo "npm install 安装最新依赖"
   npm install --prefix /scripts
 fi
