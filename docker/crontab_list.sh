9ae02f537a1e9c0396b03c1fe4cd5427d2aa6849
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2021-09-10 09:24:54 +0800

    定时任务调整

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -35,7 +35,7 @@
 #真·抢京豆
 0 0 * * *  node /scripts/jd_angryBean.js   >> /scripts/logs/jd_angryBean.log 2>&1
 #京喜领88元红包
-4 2,10 * * *  node /scripts/jd_jxlhb.js  >> /scripts/logs/jd_jxlhb.log 2>&1
+4 10,16,21,16,21 * * *  node /scripts/jd_jxlhb.js  >> /scripts/logs/jd_jxlhb.log 2>&1
 #京喜签到
 30 0,22 * * * node /scripts/jx_sign.js  >> /scripts/logs/jx_sign.log 2>&1
 #来电好物季
