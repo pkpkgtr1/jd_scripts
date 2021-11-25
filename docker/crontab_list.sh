55dde57d0789c0198b8f703803033ce06a8f141a
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2021-11-25 13:24:13 +0800

    删除

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -102,8 +102,7 @@
 0 0,6-22/2 * * *  node /scripts/jd_foodRunning.js >> /scripts/logs/jd_foodRunning.log 2>&1  
 #京东金融-每日签到
 49 8 * * * node /scripts/jd_jdjrjf.js >> /scripts/logs/jd_jdjrjf.log 2>&1 
-#东东农村-大风车
-49 7 * * * node /scripts/jd_ddnc_farmpark.js >> /scripts/logs/jd_ddnc_farmpark.log 2>&1 
+
 
 ##############长期活动##############
 # 签到
