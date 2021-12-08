f679ed04c0fbfbdbb8cc7c36f0b7223eb4568679
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2021-12-08 15:52:22 +0800

    更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -115,7 +115,8 @@
 10 4 * * * node /scripts/jd_lottery_drew.js >> /scripts/logs/jd_lottery_drew.log 2>&1 
 #星系牧场
 1 0-23/4 * * *  node /scripts/jd_qqxing.js >> /scripts/logs/jd_qqxing.log 2>&1 
-
+#京东生鲜每日抽奖
+30 7 * * *  node /scripts/jd_sxLottery.js >> /scripts/logs/jd_sxLottery.log 2>&1 
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
