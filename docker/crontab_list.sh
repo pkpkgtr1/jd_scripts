3f028bd908de3abfbabae2071fae540e9870e3f4
重新来过²º¹² <pkpkgtr@gmail.com>
2021-12-19 13:31:36 +0800

    更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -223,8 +223,6 @@
 15 0,3,6,12,14,16,18,20,22 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
 #京东电竞经理
 41 10 * * * node /scripts/jd_djjl.js >> /scripts/logs/jd_djjl.log 2>&1
-#柠檬全民挖现金
-50 0-23/8 * * * node /scripts/jd_wxj.js >> /scripts/logs/jd_wxj.log 2>&1
 #东东乐园
 18 7 * * * node /scripts/jd_ddly.js >> /scripts/logs/jd_ddly.log 2>&1
 #早起福利
