4c033b5dde5d810f3698cb570f06b4aed3be7068
重新来过²º¹² <pkpkgtr@gmail.com>
2022-01-09 19:30:30 +0800

    fix

--- gua_opencard101.js
+++ gua_opencard101.js
@@ -37,9 +37,9 @@ cron:30 2 27,9-26/3 1 *
 30 2 27,9-26/3 1 * https://raw.githubusercontent.com/smiek2121/scripts/master/gua_opencard101.js, tag=1.8~1.26 集年味卡，享年味盛宴, enabled=true
 
 */
-let guaopencard_addSku = "false"
-let guaopencard = "false"
-let guaopencard_compositeCard = "false" // 集卡 默认不集卡 需要请设置"true"
+let guaopencard_addSku = "true"
+let guaopencard = "true"
+let guaopencard_compositeCard = "true" // 集卡 默认不集卡 需要请设置"true"
 let guaopenwait = "0"
 let guaopencard_draw = "0"
 
