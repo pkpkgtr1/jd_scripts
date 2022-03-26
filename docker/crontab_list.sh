d10cc2f2636f397484ef8d3be7f11639fd037d45
重新来过²º¹² <pkpkgtr@gmail.com>
2022-03-26 17:15:54 +0800

    更新极速签到

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -200,7 +200,8 @@
 #京东保价
 #41 0,23 * * * node /scripts/jd_price.js >> /scripts/logs/jd_price.log 2>&1
 #京东极速版签到+赚现金任务
-21 1,6 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
+#21 1,6 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
+22 1,6 * * * node /scripts/jd_js_sign.js >> /scripts/logs/jd_js_sign.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
 #京喜工厂商品列表详情
