b359385f249e595c2afbea396781cd2c1abbd9ef
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2022-04-05 00:20:27 +0800

    更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -232,7 +232,7 @@
 #汪汪乐园每日任务
 20 7,9,17,20 * * * node /scripts/jd_joy_park_task.js >> /scripts/logs/jd_joy_park_task.log 2>&1 
 #汪汪乐园
-20 0-23/10 * * * node /scripts/jd_joy_park.js >> /scripts/logs/jd_joy_park.log 2>&1 
+#20 0-23/10 * * * node /scripts/jd_joy_park.js >> /scripts/logs/jd_joy_park.log 2>&1 
 #取关主播
 55 6 * * *  node /scripts/jd_unsubscriLive.js >> /scripts/logs/jd_unsubscriLive.log 2>&1
 #京东汽车兑换
