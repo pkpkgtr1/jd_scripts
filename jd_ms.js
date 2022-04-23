ee380c31ff02c0888c50ca09787f68db3089eb3d
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2022-04-23 19:26:50 +0800

    Update jd_ms.js

--- jd_ms.js
+++ jd_ms.js
@@ -4,7 +4,6 @@ Last Modified time: 2022-1-21
 活动入口：京东 首页秒杀
 更新地址：jd_ms.js
 
-
 已支持IOS双京东账号, Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, 小火箭，JSBox, Node.js
 ============Quantumultx===============
