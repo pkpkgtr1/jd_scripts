486a1a50716dda53f89a24eb1a4c0679357927dd
pkpkgtr1 <48145298+pkpkgtr1@users.noreply.github.com>
2022-05-28 09:40:26 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -75,6 +75,15 @@
 31 7-20/1 1-30 5 *   node /scripts/jd_mnyyn.js  >> /scripts/logs/jd_mnyyn.log 2>&1
 #探味奇遇记 活动入口：美食馆-右侧悬浮
 31 0,13 26-31,1-16 5,6 *   node /scripts/jd_tanwei.js  >> /scripts/logs/jd_tanwei.log 2>&1
+#京东电器巨超值
+0 20 * * *  node /scripts/jd_strategy.js  >> /scripts/logs/jd_strategy.log 2>&1
+
+##############618活动##############
+#热爱奇旅互助版-部分加密
+30,6-21/5 * * *  node /scripts/jd_19E_help.js  >> /scripts/logs/jd_19E_help.log 2>&1
+#热爱奇旅组队升级
+1 8,12 * * *   node /scripts/jd_19E_friends.js  >> /scripts/logs/jd_19E_friends.log 2>&1
+
 
 ##############开卡活动##############
 #5.5-6.20【金领冠】航天活动
@@ -87,12 +96,8 @@
 33 19 1-31 5 *  node /scripts/jd_opencardL134.js >> /scripts/logs/jd_opencardL134.log 2>&1
 #5.8-5.31 洁净星愿
 33 3 8-31 5 * node /scripts/jd_opencardL135.js >> /scripts/logs/jd_opencardL135.log 2>&1
-#5.15-5.28 童装童鞋盛典
-35 5 16-28 5 * node /scripts/jd_opencardL144.js >> /scripts/logs/jd_opencardL144.log 2>&1
 #5.17-6.1 爱上夏日 大牌献礼
 0 2 17-31,1 5,6 * node /scripts/jd_opencardL147.js >> /scripts/logs/jd_opencardL147.log 2>&1
-# 5.20~5.28 甜蜜派对 五月宠爱季
-32 12 21-28 5 * node /scripts/jd_opencardL151.js >> /scripts/logs/jd_opencardL151.log 2>&1
 #5.20~5.31 大牌优惠趴 五月爱相伴
 32 1 21-31 5 * node /scripts/jd_opencardL152.js >> /scripts/logs/jd_opencardL152.log 2>&1
 #5.23-6.20 会员联合狂欢  618百万京豆大派送
@@ -222,6 +227,6 @@
 #取关主播
 55 6 * * *  node /scripts/jd_unsubscriLive.js >> /scripts/logs/jd_unsubscriLive.log 2>&1 
 #（快过期）京豆兑换为喜豆
-33 0 * * *  node /scripts/jd_exchangejxbeans.js >> /scripts/logs/jd_exchangejxbeans.log 2>&1  
+#33 0 * * *  node /scripts/jd_exchangejxbeans.js >> /scripts/logs/jd_exchangejxbeans.log 2>&1  
 #清理购物车
 33 0 */1 * *  node /scripts/jd_cleancart.js >> /scripts/logs/jd_cleancart.log 2>&1 
